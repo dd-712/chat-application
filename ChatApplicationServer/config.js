dcc7a7e90df12eccf08cae10774177593ce1c24a
Hrushi Patel <patelhrushi16@gmail.com>
2021-12-17 22:14:11 +0530

    Initialized_server

---
+++ ChatApplicationServer/config.js
@@ -0,0 +1,4 @@
+module.exports={
+    'secretKey':'12345-67890-09876-54321',
+    'mongoUrl':'mongodb+srv://ChatApplication:ChatApplication@cluster0.eh3y8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
