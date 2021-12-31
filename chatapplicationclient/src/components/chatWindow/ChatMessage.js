750b4e49d1f8ab8aa21eab1301f2952759e5387e
dd-712 <divya807950@gmail.com>
2021-12-31 14:53:45 +0530

    msg style updated again

--- chatapplicationclient/src/components/chatWindow/ChatMessage.js
+++ chatapplicationclient/src/components/chatWindow/ChatMessage.js
@@ -8,10 +8,21 @@ function ChatMessage(props) {
     const [msgType, setMsgType] = useState('msg');
 
     return (
-        <div className={`msg ${user}`} >
-            <div className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus nunc accumsan tincidunt rutrum. Proin viverra, nunc a hendrerit facilisis, lectus sapien dapibus urna, ut consequat mi mi vitae leo. Nunc sed velit ac lectus pellentesque dictum. Vestibulum venenatis, quam nec commodo sagittis, ipsum ipsum mattis arcu, at consequat erat mi ut quam. Donec vehicula, sapien a malesuada volutpat, nunc risus lacinia mi, vitae volutpat odio ex et lorem. Maecenas varius metus sed lacinia gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id tincidunt justo.</div>
-            <div className='time'> time</div>
-        </div >
+        <div>
+            <div className={`msg ${user}`} >
+                <div className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus nunc accumsan tincidunt rutrum. Proin viverra, nunc a hendrerit facilisis, lectus sapien dapibus urna, ut consequat mi mi vitae leo. Nunc sed velit ac lectus pellentesque dictum. Vestibulum venenatis, quam nec commodo sagittis, ipsum ipsum mattis arcu, at consequat erat mi ut quam. Donec vehicula, sapien a malesuada volutpat, nunc risus lacinia mi, vitae volutpat odio ex et lorem. Maecenas varius metus sed lacinia gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id tincidunt justo.</div>
+                <div className='time'> time</div>
+            </div >
+            <div className={`msg ${user}`} >
+                <div className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus nunc accumsan tincidunt rutrum. Proin viverra, nunc a hendrerit facilisis, lectus sapien dapibus urna, ut consequat mi mi vitae leo. Nunc sed velit ac lectus pellentesque dictum. Vestibulum venenatis, quam nec commodo sagittis, ipsum ipsum mattis arcu, at consequat erat mi ut quam. Donec vehicula, sapien a malesuada volutpat, nunc risus lacinia mi, vitae volutpat odio ex et lorem. Maecenas varius metus sed lacinia gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id tincidunt justo.</div>
+                <div className='time'> time</div>
+            </div >
+            <div className={`msg ${user}`} >
+                <div className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus nunc accumsan tincidunt rutrum. Proin viverra, nunc a hendrerit facilisis, lectus sapien dapibus urna, ut consequat mi mi vitae leo. Nunc sed velit ac lectus pellentesque dictum. Vestibulum venenatis, quam nec commodo sagittis, ipsum ipsum mattis arcu, at consequat erat mi ut quam. Donec vehicula, sapien a malesuada volutpat, nunc risus lacinia mi, vitae volutpat odio ex et lorem. Maecenas varius metus sed lacinia gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id tincidunt justo.</div>
+                <div className='time'> time</div>
+            </div >
+        </div>
+
     );
 }
 
