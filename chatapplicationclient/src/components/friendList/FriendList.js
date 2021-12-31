c00bb43812fa006af1ce945384caf2b5483f822f
Hrushi Patel <patelhrushi16@gmail.com>
2021-12-31 19:50:14 +0530

    Post Connection

--- chatapplicationclient/src/components/friendList/FriendList.js
+++ chatapplicationclient/src/components/friendList/FriendList.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { baseUrl } from '../../shared/baseUrl';
@@ -7,14 +9,37 @@ import Friend from './Friend';
 
 function FriendList(props) {
 
-    
+    const [modelOpen, setState] = useState(false);
+    const [username, setUserName] = useState();
 
+    function toggleModal() {
+        setState(!modelOpen);
+    }
+    function addFriend(event) {
+        toggleModal();
+        props.postFriends(username);
+        window.location.reload(false);
+        event.preventDefault();
+    }
     return (
         <div className='friendListDiv'>
             <div className='friendListHeader'>
                 <input type='text' placeholder='Search Friends' className='input' />
                 <div className='searchIcon'><img src='/search-icon.png' width='20px' /></div>
-                <div className='friendRequest'><img src='/person-icon.png' width='20px' /></div>
+                <div className='friendRequest' onClick={toggleModal}><img src='/person-icon.png' width='20px' /></div>
+                <Modal  isOpen={modelOpen} toggle={toggleModal}>
+                    <ModalHeader toggle={toggleModal}>Add Friend</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={addFriend}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    onChange={e => setUserName(e.target.value)} />
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Add</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </div>
             <div>
                 <Friend deleteFriend={props.deleteFriend} className='friendList'/>
